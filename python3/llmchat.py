fa2eb8f444dc665af31fb5168d6263ccd69f0843
Filip Sedlak <sedlak@flih.cz>
2024-02-28 22:34:42 +0100

    wip

--- python3/llmchat.py
+++ python3/llmchat.py
@@ -15,7 +15,10 @@ def get_api_key():
 
 
 class Parser:
-    OPTIONS = ['MODEL']
+    SKIP_LINE = re.compile(r'^\s*(#.*|$)')
+    OPTION_LINE = re.compile(r'^\s*([a-zA-Z0-9]+)\s+([^\s]+)$')
+    OPTIONS = ['model']
+    SYSTEM_LINE = re.compile(r'^\s*SYSTEM:\s*(.*?)\s*$')
 
     def __init__(self, buf):
         self.buf = buf
@@ -23,6 +26,7 @@ class Parser:
         self.char = 0
         self.options = {'model': 'gpt-3.5-turbo'}
         self.messages = []
+        self.errors = []
 
     def start(self):
         self.parse_options()
@@ -30,7 +34,57 @@ class Parser:
         self.parse_conversation()
 
     def parse_options(self):
+        while self.line < len(self.buf):
+            line = self.buf[self.line]
+            self.line += 1
+            if self.SKIP_LINE.match(line):
+                continue
+
+            if m := self.OPTION_LINE.match(line):
+                if m.group(1).lower() in self.OPTIONS:
+                    self.options[m.group(1).lower()] = m.group(2)
+                    continue
+                else:
+                    self.errors.append((self.line, f'Unknown option {m.group(1)}'))
+            else:
+                self.line -= 1
+                break
+
+    def parse_system(self):
+        system_message = []
         line = self.buf[self.line]
+        self.line += 1
+        if m := self.SYSTEM_LINE.match(line):
+            system_message.append(m.group(1))
+        else:
+            self.line -= 1
+            return
+
+
+        while not line.startswith('>') and self.line < len(self.buf):
+            line = self.buf[self.line]
+            self.line += 1
+            if line.startswith('>'):
+                self.line -= 1
+                break
+            system_message.append(line.strip())
+            line = self.buf[self.line]
+
+        self.messages.append({'role': 'system', 'content': '\n'.join(system_message)})
+
+    def parse_conversation(self):
+        while self.line < len(self.buf):
+            line = self.buf[self.line]
+            self.line += 1
+
+        if line.strip()
+
+
+
+
+
+
+
 
 
 def communicate():
